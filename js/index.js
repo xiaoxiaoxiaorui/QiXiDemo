7f946773c702506ab4a2550e98ac2ea07a323c23
xiaoxiaoxiaorui <7743600989@qq.com>
2017-11-17 18:50:13 +0800

    fix(新功能): 小男孩加入
    添加小男孩及相关运动和位置

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
  * Created by Administrator on 2017/11/10.
  */
 $(function () {
+
+    //  设置页面的移动
    var container = $('.content'),
        element = $('.content-wrap'),
        slids = $('.content-wrap li'),
@@ -12,11 +14,30 @@ $(function () {
        var slide = slids.eq(index);
        slide.css({width:Width+'px',height:Height+'px'});
    });
-   $('button').on('click',function () {
+   $('.button').on('click',function () {
        element.css({
            'transition-timing-function': 'linear',
            'transition-duration': '5000ms',
            'transform': 'translate3d(-' + (Width * 2) + 'px,0px,0px)' //设置页面X轴移动
        });
-   })
+   });
+
+   //  设置小男孩的移动以及位置
+   var $boy = $('.boy');
+   $('.btn').on('click',function () {
+       // 小男孩开始运动
+       $('.boy').addClass('walking');
+       // 使用插件实现小男孩的前进
+       $boy.transition({
+           'left': container.width() + 'px'
+       },10000,'linear',function () {});
+   });
+   // 小男孩停止前进和运动
+    $('.btnStop').click(function () {
+        var Left = $boy.position().left;
+        $boy.addClass('walkpaused');
+        $boy.css({left:Left});
+    });
+    // 小男孩恢复运动
+    $boy.removeClass('walkpaused');
 });
